d23e3aaf4304f723780349a2400712e443ed0d25
shane boer <99042114@mydavinci.nl>
2021-09-10 12:00:11 +0200

    berekeningen

---
+++ de-speelhal-dag.py
@@ -0,0 +1,9 @@
+entreekosten = 7.45
+vipVR = 0.37/5
+deeltijd = 45
+personen = 3
+
+print((personen*entreekosten)+(deeltijd*vipVR))
+
+
+
---
+++ feestlunch.py
@@ -0,0 +1,7 @@
+crossaint = 0.39
+stokbrood = 2.78
+kortingsbon = 1.50
+hoeveelheid = 17
+aantal = 2
+
+print((hoeveelheid*crossaint)+(aantal*stokbrood)-kortingsbon)

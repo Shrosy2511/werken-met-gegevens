3e305f815f24636399d63ce6efebf1151ac7d64d
Shane Boer <shane2511@live.nl>
2021-09-11 10:35:17 +0200

    opdracht f1.2.02.l3

--- de-speelhal-dag.py
+++ de-speelhal-dag.py
@@ -2,8 +2,9 @@ entreekosten = 7.45
 vipVR = 0.37/5
 deeltijd = 45
 personen = 3
+factuurkosten = 'dit geweldige dagje uit met 4 mensen in de peelhal met 45 minuten VR kost je maar ' + str((personen) * (entreekosten) + (deeltijd) * (vipVR)) + ' euro'
 
-print((personen*entreekosten)+(deeltijd*vipVR))
+print(factuurkosten)
 
 
 
--- feestlunch.py
+++ feestlunch.py
@@ -3,5 +3,6 @@ stokbrood = 2.78
 kortingsbon = 1.50
 hoeveelheid = 17
 aantal = 2
+factuurtekst = 'de feestlunch kost bij de bakker ' + str((hoeveelheid) * (crossaint) + (aantal) * (stokbrood) - (kortingsbon)) + ' euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!'
 
-print((hoeveelheid*crossaint)+(aantal*stokbrood)-kortingsbon)
+print(factuurtekst)
